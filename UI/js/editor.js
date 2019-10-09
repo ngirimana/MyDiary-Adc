28148d58f5976d2452a3417555e95e9e4e2ff32c
ngirimana <chadrackngirimana@gmail.com>
2019-10-09 20:41:51 +0200

    feat(addentry): add entry page
    - add entry page
    
    - [Finishes #168749081]

---
+++ UI/js/editor.js
@@ -0,0 +1,18 @@
+const openTab = (evt, tabName) => {
+  if (tabName == 'preview') {
+    const txt = document.querySelector('#entryContent');
+
+    document.querySelector('#preview').innerHTML = txt.value;
+  }
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
