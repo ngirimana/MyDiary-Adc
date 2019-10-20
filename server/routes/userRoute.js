59150614140f3336f31acc2ca2c1ae3687c61114
ngirimana <chadrackngirimana@gmail.com>
2019-10-20 23:20:58 +0200

    feat(signinn): add signin api
    - add signin api
    - add coverage badge to readme
    - add travis badge to readme
    - add mentainability badge to readme
    
    [Finishes #169249896]

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import UserController from '../controllers/userController';
-import { signUpValidation } from '../middlewares/userValidator';
+import { signUpValidation, signInValidation } from '../middlewares/userValidator';
 
 
 const router = express.Router();
 
 
 router.post('/signup', signUpValidation, UserController.signUp);
+router.post('/signin', signInValidation, UserController.signIn);
 
 export default router;
