cb81174df995419b1c52939aef7aead2671c2739
NGIRIMANA Schadrack <38111965+ngirimana@users.noreply.github.com>
2019-10-29 15:02:34 +0200

    #169419811 remove button on read more icon and click add event listener to load entry content page (#25)
    * chore(configuration): add babel,eslint and environment variables (#12)
    
    - add eslint settings
    - add babel settings
    - add environmental variables settings
    
    - [Finishes #169249356]
    
    * feat(signup): add signup api (#13)
    
    - add signup endpoint
    - add signup test
    
    [Finishes #169249501]
    
    *  169249724 Travis and coveralls  (#14)
    
    * chore(travis):add travis configuration
    
    - add travis
    
    [Starts #169249724]
    
    * chore(coveralls): add coveralls
    
    - add coveralls configuration file
    
    [Finishes #169249724]
    
    * feat(signinn): add signin api (#15)
    
    - add signin api
    - add coverage badge to readme
    - add travis badge to readme
    - add mentainability badge to readme
    
    [Finishes #169249896]
    
    * feat(entry): add entry into diary (#16)
    
    - add add entry endpoint
    - add its test
    
    [Finishes #169253041]
    
    * feat(modify-entry): add modify an entry api (#17)
    
    - add modify entry API
    
    - [Finishes #169254421]
    
    * feat(allEntries): add get all entries endpoint (#18)
    
    - add get all entries api
    - add test for this endpoint
    
    -[Finishes #169266292]
    
    * feat(specificEntry): user can get specific entry in his/her diary (#19)
    
    - add get specific entry
    - add test for endpoint
    
    - [Finishes #169267355]
    
    * feat(deleteEntry): user should be able to deete an entry in diary (#20)
    
    - add entry enpoint
    - add test for endpoint
    
    - [Finishes #169268519]
    
    * chore(documentation): add  api documentation with swagger (#21)
    
    - add app.json for api documentation
    
    [Finishes #169270990]
    
    * chore(readme): add readme file for explaining more about project (#22)
    
    - add readme file
    
    - [Finishes #169295474]
    
    * chore(feadback): add click event to eye icon for read more
    
    - add event listener to read more icon
    
    - [Finishes #169419811]
    
    * chore(feedback): remove server files
    
    - remove server related files
    
    -[Finishes #169419811]

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MyDiary-Adc
+
 MyDiary is an online journal where users can pen down their thoughts and feelings.
+
---
+++ UI/js/btn.js
@@ -0,0 +1,8 @@
+
+const readMore = () => {
+  // document.querySelector('.read-more').addEventListener('click', () => {
+    window.location.href = '../html/entry-content.html';
+  // });
+
+
+}
