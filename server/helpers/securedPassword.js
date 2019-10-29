18684bc9b19a3a1bd04bed2735e3e33aba10ac53
ngirimana <chadrackngirimana@gmail.com>
2019-10-29 14:47:27 +0200

    chore(feedback): remove server files
    - remove server related files
    
    -[Finishes #169419811]

--- server/helpers/securedPassword.js
+++
@@ -1,4 +0,0 @@
-import bcrypt from 'bcrypt';
-
-export const encryptPassword = (pswd) => bcrypt.hashSync(pswd, Number(process.env.PASSWORD_SALT));
-export const decryptPassword = (userPswd, hashedPswd) => bcrypt.compareSync(userPswd, hashedPswd);
