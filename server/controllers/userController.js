107b4dc39563fddde2221bb752a11e18f02d5fb9
ngirimana <chadrackngirimana@gmail.com>
2019-11-02 22:10:03 +0200

    feat(signin): add signin api using database
    - add signin endpoint
    - add test
    - add signin validation
    
    -[Finishes #169519647]

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import Model from '../models/db';
 import { generateAuthToken } from '../helpers/token';
 import response from '../helpers/responses';
-import { encryptPassword } from '../helpers/securedPassword';
+import { encryptPassword, decryptPassword } from '../helpers/securedPassword';
 
 dotenv.config();
 
@@ -36,6 +36,24 @@ class UserController {
       return response.successResponse(res, 201, 'User created successfully', data);
     } catch (error) {
       return response.errorResponse(res, 500, error);
+    }
+  }
+
+  static signIn = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await this.model().select('*', 'email=$1', [email]);
+      if (user[0] && (decryptPassword(password, user[0].password))) {
+        const token = generateAuthToken(user[0].id, user[0].email);
+        const data = {
+          token,
+          userData: lodash.pick(user[0], 'id', 'firstname', 'lastname', 'email'),
+        };
+        return response.successResponse(res, 200, 'User signed in successfully', data);
+      }
+      return response.errorResponse(res, 401, 'Incorrect email or password');
+    } catch (err) {
+      return response.errorResponse(res, 500, err);
     }
   }
 }
